3b7d3929c30fd712aee49d356c8fed9b81d893c8
priyanka5123 <priyanka.k8100@gmail.com>
2026-01-21 13:37:21 -0700

    modified events

--- src/PokemonEvents.jsx
+++ src/PokemonEvents.jsx
@@ -1,4 +1,4 @@
-import { use, useState } from "react";
+import { useState } from "react";
 function PokemonEvents(){
     const [inputValue, setInputValue] = useState('');
     const [eventStatus, setEventStatus] = useState('Pokemon Finder');
